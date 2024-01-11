8b94e1c77b0ca1e8641e6299b12f175ed1b74c2e
Mathews <mathewsulike20@gmail.com>
2024-01-11 18:08:31 +0530

    hey there

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import "./App.css";
 import InputFeild from "./components/InputFeild";
+import {Todo} from "./components/model";
 
 
 const App: React.FC= ()=>{
 
+  const [todo,setTodo] =useState<string>("");
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+  console.log(todo)
   return(
     <div className='App'>
       <span className='heading'>Taskify</span>
-      <InputFeild/>
+      <InputFeild todo={todo} setTodo={setTodo}/>
     </div>
   )
 }
--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./styles.css";
 
-const InputFeild=()=>{
-
-
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+const InputFeild:React.FC <Props>=({todo , setTodo}) => {
     return (
        <form className='input'>
-        <input type="input" placeholder='Enter the task ...' className='input_box' />
+        
+        <input type="input" value={todo} 
+        onChange={(e) =>setTodo(e.target.value)} placeholder='Enter the task ...' className='input_box' />
         <button className="input_submit" type='submit'>Go</button>
        </form>
     )
