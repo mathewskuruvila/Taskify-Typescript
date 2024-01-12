8ba0b58e065ebba3a871b34b08f406737c2a77ce
Mathews <mathewsulike20@gmail.com>
2024-01-12 13:14:05 +0530

    heyyy

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,15 +9,16 @@ interface Props {
 
 }
 
-const TodoList :React.FC = ({todos,setTodos}:Props)=>{
+const TodoList :React.FC<Props> = ({todos,setTodos}:Props)=>{
 
 
 
-    return <div className="todos">
-        {todos.map}(todo=> (
+    return (
+    <div className="todos">
+        {todos.map((todo) => (
             <li>{todo.todo}</li>
-        ))
-        </div>;
-    
-}
+        ))}
+        </div>
+    );
+};
 export default TodoList
