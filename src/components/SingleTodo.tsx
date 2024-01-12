8a01f0a4a62082c7e68c9f45e19945cc7eb15ec5
Mathews <mathewsulike20@gmail.com>
2024-01-12 14:42:06 +0530

    heyy3ya1

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,20 +1,30 @@
 import react from "react";
-import {Todo} from "../models/model"
-
+import { Todo } from "../models/model";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete, MdDoneOutline } from "react-icons/md";
 
 type Props = {
-    todo:Todo;
-    todos:Todo[];
-    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-
-const SingleTodo = ({todo,todos,setTodos})=>{
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     return (
-        <div>
-
-        </div>
-    )
-
-}
+        <form className="todos_single">
+            <span className="todos_single--text">{todo.todo}</span>
+            <div>
+                <span className="icon">
+                    <FaEdit />{" "}
+                </span>
+                <span className="icon">
+                    <MdDelete />{" "}
+                </span>
+                <span className="icon">
+                    <MdDoneOutline />
+                </span>
+            </div>
+        </form>
+    );
+};
 export default SingleTodo;
